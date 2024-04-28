822ad6728393ddcb631002aa482b3d4833ac8db9
nokibh <nokibhowlader2003@gmail.com>
2024-04-28 13:09:42 +0600

    added env

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q0w7vnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
