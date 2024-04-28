7088729f0e4bcd3eb0ad3186e94c7d6dc4f4d005
nokibh <nokibhowlader2003@gmail.com>
2024-04-28 22:27:42 +0600

    added mylist final

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run() {
       const result = await addCollection.insertOne(addAll);
       res.send(result);
     });
+    app.get('/mylist/:email', async (req, res) => {
+      console.log(req.params.email);
+      const result = await addCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
