4e2546861e81f9a10dca4f905e20ed06519439bc
nokibh <nokibhowlader2003@gmail.com>
2024-04-30 16:11:43 +0600

    added new server

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run() {
       const result = await addCollection.insertOne(addAll);
       res.send(result);
     });
+    // country
+    app.get('/country', async (req, res) => {
+      const cursor = addCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // My list
     app.get('/mylist/:email', async (req, res) => {
       console.log(req.params.email);
       const result = await addCollection
